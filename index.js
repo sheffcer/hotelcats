d8756ad162f1dab141d55120621939bc7ed60572
Alex sheffcer <a.sheffcer@gmail.com>
2020-01-22 17:11:03 +0200

    Update 2020-01-22T15:11:00.352Z

---
+++ index.js
@@ -0,0 +1,50 @@
+let navToggle = document.querySelector('.nav-toggle');
+let navLinks = document.querySelectorAll('.main-nav__link')
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+      document.body.classList.remove('nav-open');
+  })
+})
+$('#owl').owlCarousel({
+  // items: 5,
+  merge: true,
+  loop: true,
+  margin: 10,
+  lazyLoad: true,
+  autoplay: true,
+  responsiveClass: true,
+  responsive: {
+      0: {
+          items: 1,
+          nav: false,
+          dots: true,
+          loop: true,
+      },
+      320: {
+          items: 1,
+          nav: false,
+          dots: true
+      },
+      // 480: {
+      //     items: 2,
+      //     nav: false,
+      //     dots: false
+      // },
+      768: {
+          items: 1,
+          nav: false,
+          dots: false
+      },
+      1024: {
+          items: 1,
+          nav: false,
+          loop: true,
+      }
+  }
+}
+);

ea75abfa25c82d1431aeb8b88dfa07c3c96266fb
Alex sheffcer <a.sheffcer@gmail.com>
2020-01-23 11:25:31 +0200

    styled mobile reviews block

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ navLinks.forEach(link => {
       document.body.classList.remove('nav-open');
   })
 })
-$('#owl').owlCarousel({
+$('#rooms').owlCarousel({
   // items: 5,
   merge: true,
   loop: true,
@@ -48,3 +48,43 @@ $('#owl').owlCarousel({
   }
 }
 );
+
+$('#reviews').owlCarousel({
+  // items: 5,
+  merge: true,
+  loop: true,
+  margin: 10,
+  lazyLoad: true,
+  autoplay: true,
+  responsiveClass: true,
+  responsive: {
+      0: {
+          items: 1,
+          nav: false,
+          dots: true,
+          loop: true,
+      },
+      320: {
+          items: 1,
+          nav: false,
+          dots: true
+      },
+      // 480: {
+      //     items: 2,
+      //     nav: false,
+      //     dots: false
+      // },
+      768: {
+          items: 1,
+          nav: false,
+          dots: false
+      },
+      1024: {
+          items: 1,
+          nav: false,
+          loop: true,
+      }
+  }
+}
+);
+
